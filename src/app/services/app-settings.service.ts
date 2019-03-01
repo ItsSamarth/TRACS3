d863c755a831b329e25ec881a8d687d503c75a94
samarth <samarth19sam@gmail.com>
2019-03-01 12:32:30 +0530

    Fix(Reload) - Removed href from anchor tag

--- src/app/services/app-settings.service.ts
+++ src/app/services/app-settings.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import { AppSettings } from '../shared/app-settings';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+import { AppSettings } from "../shared/app-settings";
 import { APPSETTINGS } from "../shared/defaultSettings";
 
 @Injectable()
 export class AppSettingsService {
   getSettings(): Observable<AppSettings> {
-    let settings =  APPSETTINGS;
+    let settings = APPSETTINGS;
     return Observable.of<AppSettings>(settings);
   }
 }
